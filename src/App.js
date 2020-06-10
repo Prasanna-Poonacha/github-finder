38f95c4043621b37f8ec905aef02e005e291d88d
Prasanna Poonacha <ppoonacha@mast.com>
2020-06-10 19:38:06 +0530

    Home and NotFound component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
-import Search from "./components/users/Search";
 import User from "./components/users/User";
 import Alert from "./components/layout/Alert";
+import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
+import NotFound from "./components/pages/NotFound";
 import "./App.css";
 
 const App = () => {
@@ -15,26 +15,16 @@ const App = () => {
     <GithubState>
       <AlertState>
         <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Alert />
-              <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/user/:login" component={User} />
-              </Switch>
-            </div>
-          </Fragment>
+          <Navbar />
+          <div className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/user/:login" component={User} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
         </Router>
       </AlertState>
     </GithubState>
